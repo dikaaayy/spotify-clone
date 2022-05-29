c008840253570a5bfa1586b88748f67bec23c302
dikaaayy <andikayudhistira3@gmail.com>
2022-05-29 14:46:22 +0700

    adding position sticky to playlist

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,16 @@
 import Head from "next/head";
-
-import React from "react";
-
 export default function Header() {
   return (
     <Head>
       <title>Spotify Web Player</title>
       <link rel="icon" href="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png" type="image/x-icon" />
 
-      <link rel="canonical" href="https://spotify.andikay.me/" />
+      <link rel="canonical" href="https://spotify-player.andikay.me/" />
 
-      <meta name="description" content="Spotify Clone porfolio website made by Dika for Personal Purpose" />
-      <meta property="og:description" content="Spotify Clone porfolio website made by Dika for Personal Purpose" />
+      <meta name="description" content="Spotify Web Player porfolio website made by Dika for Personal Purpose" />
+      <meta property="og:description" content="Spotify Web Player porfolio website made by Dika for Personal Purpose" />
 
-      <meta property="og:url" content="https://spotify.andikay.me/" />
+      <meta property="og:url" content="https://spotify-player.andikay.me/" />
       <meta property="og:site_name" content="andikay" />
 
       <meta property="og:image" content="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png" />
--- components/Songs.jsx
+++ components/Songs.jsx
@@ -6,27 +6,24 @@ import { ClockIcon } from "@heroicons/react/outline";
 export default function Songs() {
   const playlist = useRecoilValue(playlistState);
   return (
-    <>
-      <div className="songs-container">
-        <div className="grid md:grid-cols-5 items-center pt-6 pb-2 pl-4 text-[#B3B3B3] text-xs font-semibold">
-          <div className="flex gap-x-9">
-            <p>#</p>
-            <p>TITLE</p>
-          </div>
-          <p className="invisible">TITLE</p>
-          <p>ALBUM</p>
-          <p className="ml-8 hidden md:block">DATE ADDED</p>
-          <ClockIcon className="w-5 h-5 justify-self-end mr-5 hidden md:block" />
+    <div className="songs-container relative">
+      <div className="grid md:grid-cols-5 items-center pt-6 pb-2 pl-4 text-[#B3B3B3] text-xs font-semibold sticky top-0 bg-[#121212] border-b-[1px] border-[#626262]">
+        <div className="flex gap-x-9">
+          <p>#</p>
+          <p>TITLE</p>
         </div>
-        <hr className="border-[#626262] border-t-[1px]" />
-        {playlist?.tracks.items.map((item, i) => {
-          return (
-            <div key={item?.track?.id}>
-              <Song order={i} key={item?.track?.id} item={item} />
-            </div>
-          );
-        })}
+        <p className="invisible">TITLE</p>
+        <p>ALBUM</p>
+        <p className="ml-8 hidden md:block">DATE ADDED</p>
+        <ClockIcon className="w-5 h-5 justify-self-end mr-5 hidden md:block" />
       </div>
-    </>
+      {playlist?.tracks.items.map((item, i) => {
+        return (
+          <div key={item?.track?.id}>
+            <Song order={i} key={item?.track?.id} item={item} />
+          </div>
+        );
+      })}
+    </div>
   );
 }
