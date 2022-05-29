e107e7536aa9ef14bb6dfd024143c669d73abfeb
dikaaayy <andikayudhistira3@gmail.com>
2022-05-29 14:49:01 +0700

    added title props to login

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,27 +1,34 @@
 import { getProviders, signIn } from "next-auth/react";
+import Head from "next/head";
 
 const logo = "https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-green-logo-8.png";
 
 export default function login({ providers }) {
   // console.log(providers);
   return (
-    <div className="flex flex-col items-center justify-center bg-black min-h-screen w-full gap-y-6">
-      <img className="w-52" src={logo} alt="spotify-logo" />
-      {Object.values(providers).map((provider) => {
-        return (
-          <div key={provider.name}>
-            <button
-              className="bg-[#1ed760] transition hover:opacity-90 py-3 px-10 rounded-full font-semibold text-[#f0f0f0] active:outline active:outline-[3px] active:outline-white active:outline-offset-[3px]"
-              onClick={() => {
-                signIn(provider.id, { callbackUrl: "/" });
-              }}
-            >
-              Login with {provider.name}
-            </button>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <Head>
+        <title>Login • Spotify Web Player</title>
+        <link rel="icon" href="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png" type="image/x-icon" />
+      </Head>
+      <div className="flex flex-col items-center justify-center bg-black min-h-screen w-full gap-y-6">
+        <img className="w-52" src={logo} alt="spotify-logo" />
+        {Object.values(providers).map((provider) => {
+          return (
+            <div key={provider.name}>
+              <button
+                className="bg-[#1ed760] transition hover:opacity-90 py-3 px-10 rounded-full font-semibold text-[#f0f0f0] active:outline active:outline-[3px] active:outline-white active:outline-offset-[3px]"
+                onClick={() => {
+                  signIn(provider.id, { callbackUrl: "/" });
+                }}
+              >
+                Login with {provider.name}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
